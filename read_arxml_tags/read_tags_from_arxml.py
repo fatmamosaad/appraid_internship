ebef05f3f020ea9751da4d3a8f37a7b75a7ce123
fatmamosaad <ext.fatma.mosaad@appraid-tech.com>
2024-05-26 13:24:32 +0300

    add readme to explain what is script do

--- read_arxml_tags/read_tags_from_arxml.py
+++ read_arxml_tags/read_tags_from_arxml.py
@@ -58,5 +58,5 @@ arxml_file_path = get_file_path()  # Get the file path from the user
 
 extracted_data = extract_sw_base_type_data(arxml_file_path)
 output_file=r'C:\Users\ext.fatma.mosaad\Desktop\Fatma\read_xml\output.txt'
-write_file(output_file, extracted_data) 
+write_file(output_file, extracted_data) # type: ignore
 
