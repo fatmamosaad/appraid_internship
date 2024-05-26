e58095babe2204e7c0fb99e24c718b6f6ad70115
fatmamosaad <ext.fatma.mosaad@appraid-tech.com>
2024-05-26 12:57:06 +0300

    add readme to explain what is script do

--- read_arxml_tags/read_tags_from_arxml.py
+++ read_arxml_tags/read_tags_from_arxml.py
@@ -21,37 +21,6 @@ def get_file_path():
     print(f"File selected: {file_path}")
     return file_path
 
-
-
-# def extract_sw_base_type_data(arxml_file_path):
-#     try:
-#         tree = ET.parse(arxml_file_path)
-#         root = tree.getroot()
-
-#         # Assuming the namespace is defined as 'ns'
-#         ns = {'ns': 'http://autosar.org/schema/r4.0'}
-
-#         # Find all SW-BASE-TYPE elements
-#         sw_base_types = root.findall('.//ns:SW-BASE-TYPE', namespaces=ns)
-
-#         # Extract the data you need (e.g., attribute values)
-#         data_list = []
-        
-#         for sw_base_type in sw_base_types:
-#             attributes = {}
-#             for child in sw_base_type:
-#                 attributes[child.tag] = child.text
-#                 if(child.text == "\n                    "):
-#                     pass
-#                 else:
-#                     data_list.append([filter_tag(child.tag),child.text])
-
-#         return data_list
-            
-#     except Exception as e:
-#         print(f"Error parsing ARXML file: {e}")
-#         return None
-
 def extract_sw_base_type_data(arxml_file_path):
     try:
         tree = ET.parse(arxml_file_path)
@@ -88,12 +57,6 @@ arxml_file_path = get_file_path()  # Get the file path from the user
 
 
 extracted_data = extract_sw_base_type_data(arxml_file_path)
-
-# listOfDicts = [] 
-
-# for i in range(0,len(extracted_data)-5,5):
-    
-#      listOfDicts.append(dict(shortName = extracted_data[i][1],category = extracted_data[i+1][1],baseTypeSize = extracted_data[i+2][1],baseTypeEncoding = extracted_data[i+3][1],nativeDeclaration = extracted_data[i+4][1]))  
 output_file=r'C:\Users\ext.fatma.mosaad\Desktop\Fatma\read_xml\output.txt'
-write_file(output_file, extracted_data) # type: ignore
+write_file(output_file, extracted_data) 
 
